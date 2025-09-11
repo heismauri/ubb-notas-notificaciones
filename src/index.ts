233190a5732d141fa2f851cf6e890ce0ff77bb2e
Mauricio Villa <vbamauricio@gmail.com>
2025-09-11 11:06:11 -0300

    Add course notification functionality and refactor fetch handling

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import type { Course } from "@/types/Course";
 import type { MarksResponse } from "@/types/MarksResponse";
 
 const getCourseName = (code: string): string => {
@@ -42,8 +43,67 @@ const getMarksCount = (marksResponse: MarksResponse) => {
   }).length;
 };
 
+const getCourseMessage = (course: Course) => {
+  const courseName = getCourseName(course.code);
+  return `El ramo **"${courseName}"** (${course.code}) acaba de subir nuevas notas`;
+};
+
+const genPayload = (messages: string[]) => {
+  return {
+    content: null,
+    embeds: [
+      {
+        title: "¡Nuevas notas disponibles!",
+        description: messages.join("\n"),
+        color: 84120,
+      },
+    ]
+  };
+};
+
+const sendNotification = async (
+  env: Env,
+  payload: { content: string | null; embeds: { title: string; description: string; color: number }[] }
+) => {
+  const response = await fetch(env.DISCORD_WEBHOOK_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+  if (!response.ok) {
+    throw new Error("Failed to send notification");
+  }
+};
+
+const handleFetch = async (env: Env) => {
+  const coursesKV = await env.ubbnotas.get("courses");
+      const courses: Course[] = coursesKV ? JSON.parse(coursesKV) : [];
+      const newMarkMessages: string[] = [];
+      await Promise.all(
+        courses.map(async (course) => {
+          const response = await fetchMarks(course, env);
+          const marksCount = getMarksCount(response);
+          if ((course.marksCount || 0) < marksCount) {
+            course.marksCount = marksCount;
+            newMarkMessages.push(getCourseMessage(course));
+          }
+        })
+      );
+      if (newMarkMessages.length > 0) {
+        const payload = genPayload(newMarkMessages);
+        await sendNotification(env, payload);
+      }
+      return new Response(JSON.stringify({ success: true }), { status: 200 });
+}
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
+  async fetch(_, env): Promise<Response> {
+    try {
+      return handleFetch(env);
+    } catch (error) {
+      return new Response(JSON.stringify({ error }), { status: 500 });
+    }
+  },
 } satisfies ExportedHandler<Env>;
