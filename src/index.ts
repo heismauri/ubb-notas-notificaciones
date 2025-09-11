d5a841d909beaf1641b81dbef18827ceb20350d8
Mauricio Villa <vbamauricio@gmail.com>
2025-09-11 07:31:09 -0300

    Replace tabs with spaces

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,38 @@
 import type { MarksResponse } from "@/types/MarksResponse";
 
 const getCourseName = (code: string): string => {
-	switch (code) {
-		default:
+  switch (code) {
+    default:
       return "Nombre de asignatura desconocida";
-	}
+  }
 };
 
 const fetchMarks = async (
-	{
-		code,
-		semester,
-		year,
-		section,
-		other,
-		modular = false,
+  {
+    code,
+    semester,
+    year,
+    section,
+    other,
+    modular = false,
   }: { code: string; semester: string; year: string; section: string; other: string; modular?: boolean },
-	env: Env
+  env: Env
 ): Promise<MarksResponse> => {
-	const endpoint = modular ? env.MODULAR_MARKS_ENDPOINT : env.MARKS_ENDPOINT;
+  const endpoint = modular ? env.MODULAR_MARKS_ENDPOINT : env.MARKS_ENDPOINT;
   const response = await fetch(`${endpoint}/${env.RUN}/${code}/${section}/${year}/${semester}/${other}`, {
-		headers: {
+    headers: {
       Accept: "*/*",
       "User-Agent": "YoSoyUBB/48 CFNetwork/3826.600.41 Darwin/24.6.0",
       "Accept-Language": "en-US,en;q=0.9",
-			Authorization: `Bearer ${env.TOKEN}`,
+      Authorization: `Bearer ${env.TOKEN}`,
       Pragma: "no-cache",
       "Cache-Control": "no-cache",
-		},
-	});
-	if (!response.ok) {
+    },
+  });
+  if (!response.ok) {
     throw new Error("Failed to fetch marks");
-	}
-	return response.json();
+  }
+  return response.json();
 };
 
 export default {
