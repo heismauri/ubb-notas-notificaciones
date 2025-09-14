c8711afcc28d23fa75b1e44aafceae7a86369583
Mauricio Villa <vbamauricio@gmail.com>
2025-09-14 13:07:12 -0300

    Add ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{ts,mts,cts}"], languageOptions: { parser: tseslint.ESLintParser } },
+  globalIgnores(["worker-configuration.d.ts", "test/**"]),
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  eslintConfigPrettier,
+  {
+    rules: {
+      "max-len": ["error", { code: 120, ignoreComments: true }],
+      "no-console": "warn",
+    }
+  }
+]);
