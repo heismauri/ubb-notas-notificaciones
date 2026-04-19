ca2ad9ec674c3300ca6e7a33af45bbc14104f33b
Mauricio Villa <vbamauricio@gmail.com>
2026-04-19 11:41:40 -0400

    Fix tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,6 +1,6 @@
 import worker from "@/index";
 import { env, fetchMock } from "cloudflare:test";
-import { describe, expect, it, beforeAll, afterEach } from "vitest";
+import { describe, expect, it, beforeAll, beforeEach, afterEach } from "vitest";
 
 import carreras from "./mocks/carreras.json";
 import asignaturas from "./mocks/asignaturas.json";
@@ -10,40 +10,45 @@ import mICalificaciones from "./mocks/calificaciones/2201561-I.json";
 import mIICalificaciones from "./mocks/calificaciones/2201561-II.json";
 
 const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
+const run = "12749625";
 
-beforeAll(() => {
+beforeAll(async () => {
+  await env.DATA.put("students", JSON.stringify([{ run, firstName: "Test", lastName: "User" }]));
+});
+
+beforeEach(() => {
   fetchMock.activate();
   fetchMock.disableNetConnect();
 });
 
 afterEach(() => {
   fetchMock.assertNoPendingInterceptors();
+  fetchMock.enableNetConnect();
 });
 
 describe("Refresh courses", () => {
   // This test only works the first time it is run, then env.DATA.put fails
   it("responds with a success message", async () => {
-    env.RUNS = "";
     fetchMock
       .get(env.BASE_URL)
-      .intercept({ path: `/v2/config/get_carreras/${env.RUN}` })
+      .intercept({ path: `/v2/config/get_carreras/${run}` })
       .reply(200, carreras);
     fetchMock
       .get(env.BASE_URL)
-      .intercept({ path: `/calificaciones/get_asignaturas/${env.RUN}/29370/2/2025/1/2025/2` })
+      .intercept({ path: `/calificaciones/get_asignaturas/${run}/29370/2/2025/1/2025/2` })
       .reply(200, asignaturas);
     fetchMock.get(env.BASE_URL).intercept({ path: "/calificaciones/get_modulos/2201561/1/2025/2" }).reply(200, modulos);
     fetchMock
       .get(env.BASE_URL)
-      .intercept({ path: `/calificaciones/get_calificaciones/${env.RUN}/6205031/2/2025/2` })
+      .intercept({ path: `/calificaciones/get_calificaciones/${run}/6205031/2/2025/2` })
       .reply(200, aCalificaciones);
     fetchMock
       .get(env.BASE_URL)
-      .intercept({ path: `/calificaciones/get_calificaciones_modular/${env.RUN}/2201561/1/2025/2/29370/2/1/1` })
+      .intercept({ path: `/calificaciones/get_calificaciones_modular/${run}/2201561/1/2025/2/29370/2/1/1` })
       .reply(200, mICalificaciones);
     fetchMock
       .get(env.BASE_URL)
-      .intercept({ path: `/calificaciones/get_calificaciones_modular/${env.RUN}/2201561/1/2025/2/29370/2/2/1` })
+      .intercept({ path: `/calificaciones/get_calificaciones_modular/${run}/2201561/1/2025/2/29370/2/2/1` })
       .reply(200, mIICalificaciones);
 
     const request = new IncomingRequest("http://example.com/refresh-courses");
